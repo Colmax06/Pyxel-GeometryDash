595ac85d4b353d32512b02cfd1faf00f8f42d21a
elvisthegamer <elvisthegamerofficial@gmail.com>
2026-03-05 08:40:56 +0100

    speedlvl cheat

--- cheats.py
+++ cheats.py
@@ -3,7 +3,7 @@ import pyxel
 class Cheats:
     def __init__(self, game):
         self.game = game
-
+        self.speedlvl = False
         self.noclip = False
 
     def noclip_change(self):
@@ -13,10 +13,21 @@ class Cheats:
             return True
         else:
             return self.noclip
+    
+    def speedlvl_change(self):
+        if pyxel.btnp(pyxel.KEY_B):
+            if not self.speedlvl:
+                self.game.level.speed = self.game.level.velocity_x*3
+                return True
+            elif self.speedlvl:
+                self.game.level.speed = self.game.level.velocity_x
+                return False
+        else:
+            return self.speedlvl
 
     def cheats_update(self):
         self.noclip = self.noclip_change()
-
+        self.speedlvl = self.speedlvl_change()
     def cheats_draw(self):
         if self.noclip:
             pyxel.text(self.game.screen_x-30,5,"NOCLIP",8)
